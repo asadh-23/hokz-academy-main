643e1846372c5bf9d7d108986c30ede035103ce8
Asadh-23 <asadhfor2004@email.com>
2026-02-22 07:55:16 +0530

    bug fixed

--- frontend/src/pages/home/TutorLoginSection.jsx
+++ frontend/src/pages/home/TutorLoginSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {  useNavigate } from "react-router-dom";
-import TutorLoginSectionImage from "../../assets/images/TutorLoginSection.avif"
+import TutorLoginSectionImage from "../../assets/images/tutorLoginSection.avif"
 export const TutorLoginSection = () => {
   const navigate = useNavigate();
     return (
